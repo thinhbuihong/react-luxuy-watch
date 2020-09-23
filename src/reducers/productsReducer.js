161c4fd58c1d4c8cb0b790ce41fceb08840a832a
thinhbuihong <thinhbuihong.ma@gmail.com>
2020-09-23 16:51:20 +0700

    filter + sort

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,11 +1,23 @@
-import { FETCH_PRODUCTS } from "../constants/actionTypes";
+import { FETCH_PRODUCTS, SEARCH_PRODUCTS, SORT_PRODUCTS } from "../constants/actionTypes";
 import data from '../data.json';
 
 const productsReducer= (state=[], action)=>{
   switch(action.type){
     case FETCH_PRODUCTS:
       return data
+
+    case SORT_PRODUCTS:
+      state.sort((a,b)=> {
+        let result = a.price - b.price;
+        return action.payload.order === "lowest" ? result : (-1* result) ;
+      })
+      return [...state];
     
+    case SEARCH_PRODUCTS:
+      return data.filter((product) =>{
+        return product.title.includes(action.payload.temp);
+      });
+
     default:
       return [...state];
   }
